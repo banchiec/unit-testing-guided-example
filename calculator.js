1935648134e4da3d4f02c89380b4b64af93b43ae
Ivan Espinoza <icaro_19@hotmail.com>
2021-08-12 15:37:08 +0200

    finish work

--- calculator.js
+++ calculator.js
@@ -1,13 +1,29 @@
 function sum(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0;
+  }
+  if (b === undefined) {
+    return a;
+  }
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0;
+  }
+  if (b === undefined) {
+    return a;
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+
+  if (b === 0) {
+    throw new Error('An explanatory error message');
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
